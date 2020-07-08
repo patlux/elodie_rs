8bc4a0067ffbcaca0d2c8eeb10411e037ae47405
Patrick Wozniak <email@patwoz.de>
2020-07-08 20:11:06 +0200

    refactor(app): use iterator instead for loop

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,56 @@ use std::fmt::Write;
 use std::fs;
 use std::io;
 
+struct ImageFile {
+  path_src: String,
+  sha256: String,
+}
+
 fn main() {
   let home_folder = home_dir().expect("Failed to get home folder.");
   let dir_path = format!("{}/Pictures/Camera/", home_folder.display());
-  println!("Get files in {}.", &dir_path);
+  println!("Get files in {}", &dir_path);
 
   read_sha256_dir(&dir_path).expect("Failed to get sha256 for dir.");
-  println!("{}", dir_path);
 }
 
-fn read_sha256_dir(path_dir: &str) -> io::Result<()> {
-  for entry in fs::read_dir(&path_dir)? {
-    let path = entry?.path();
-    if !path.is_file() {
-      continue;
-    }
-    let mut file_sha256sum = String::new();
-    let file_path = format!("{}", path.display());
-    let mut hasher: Sha256 = Sha256::new();
-    read_sha256(&mut hasher, &file_path, &mut file_sha256sum).expect(&format!(
-      "Failed to read sha256sum for file '{}'",
-      file_path
-    ));
-    println!("{}: {}", file_path, file_sha256sum);
+fn read_sha256_dir(path_dir: &str) -> Result<(), Box<dyn error::Error>> {
+  let mut hasher: Sha256 = Sha256::new();
+
+  let re = fs::read_dir(path_dir)?;
+  let image_files: Vec<_> = re
+    .map(|res_dir_entry| res_dir_entry.unwrap().path())
+    .filter(|path_buf| path_buf.is_file())
+    .map(|path_buf| format!("{}", path_buf.display()))
+    .filter_map(|path_str| {
+      let mut sha256 = String::new();
+      match hash_file(&mut hasher, &path_str, &mut sha256) {
+        Ok(_) => Some(ImageFile {
+          path_src: path_str,
+          sha256,
+        }),
+        Err(_) => return None,
+      }
+    })
+    .collect();
+
+  for (index, image_file) in image_files.iter().enumerate() {
+    println!(
+      "{}. {}: {}",
+      index + 1,
+      image_file.path_src,
+      image_file.sha256
+    );
   }
   Ok(())
 }
 
-fn read_sha256(
+fn hash_file(
   hasher: &mut Sha256,
-  path: &str,
+  path_to_file: &str,
   output: &mut String,
 ) -> Result<(), Box<dyn error::Error>> {
-  let mut file = fs::File::open(path)?;
+  let mut file = fs::File::open(path_to_file)?;
   io::copy(&mut file, hasher)?;
   let result = hasher.finalize_reset();
   output.write_fmt(format_args!("{:x}", &result))?;
